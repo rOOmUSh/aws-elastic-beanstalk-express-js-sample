70568600ea5f1e8880c9caeedb3b97dc910b0e4a
rOOmUSh <roomush@gmail.com>
2020-12-22 10:58:11 +0200

    pomenial huiniu 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Che pasesh, pedrila?'));
+app.get('/', (req, res) => res.send('dalshe my pili vodku'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
